9ae5773d57ed20c822f8c373114ecad7d9cc75f2
Kell Banks <mckellbanks@gmail.com>
2020-05-26 15:28:07 -0600

    set up express, and server functioning

--- FriendFinderApp/server.js
+++ FriendFinderApp/server.js
@@ -1,2 +1,15 @@
 var express = require("express");
-var express = require("path");
+var path = require("path");
+
+//Express setup
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//parsing data for json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Listening for server port
+app.listen(PORT, function () {
+    console.log("Server listening on PORT " + PORT);
+});
