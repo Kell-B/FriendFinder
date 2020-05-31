2c58435d8299f8a78df8648581dc4eb6a1f6ebfb
Kell Banks <mckellbanks@gmail.com>
2020-05-31 16:02:37 -0600

    fixed photos and added a img class

--- FriendFinderApp/app/data/friends.js
+++ FriendFinderApp/app/data/friends.js
@@ -2,42 +2,42 @@
 var friends = [
     {
         name: "Jim",
-        photo: "",
+        photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         name: "Jan",
-        photo: "",
-        scores: [1, 4, 5, 2, 1, 5, 4, 4, 1, 5]
+        photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
         name: "James",
-        photo: "",
+        photo: "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [3, 2, 5, 4, 1, 2, 3, 4, 1, 5]
     },
     {
         name: "Jane",
-        photo: "",
+        photo: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [5, 3, 2, 5, 4, 1, 2, 3, 4, 1]
     },
     {
         name: "Jason",
-        photo: "",
+        photo: "https://images.pexels.com/photos/832998/pexels-photo-832998.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [1, 5, 3, 2, 5, 4, 1, 2, 3, 4]
     },
     {
         name: "Jezebel",
-        photo: "",
+        photo: "https://images.pexels.com/photos/713312/pexels-photo-713312.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [4, 1, 5, 3, 2, 5, 4, 1, 2, 3]
     },
     {
         name: "Jake",
-        photo: "",
+        photo: "https://images.pexels.com/photos/769746/pexels-photo-769746.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [3, 4, 1, 5, 3, 2, 5, 4, 1, 2]
     },
     {
         name: "Jamie",
-        photo: "",
+        photo: "https://images.pexels.com/photos/935985/pexels-photo-935985.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         scores: [2, 3, 4, 1, 5, 3, 2, 5, 4, 1]
     }
 
