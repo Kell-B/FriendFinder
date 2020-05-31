44681c550bae4d67d77e4336d3445ffdae8c8d23
Kell Banks <mckellbanks@gmail.com>
2020-05-31 13:52:39 -0600

    fixed home page html and survey html to display and rout properly. adde more frinds to the friends.js file

--- FriendFinderApp/app/data/friends.js
+++ FriendFinderApp/app/data/friends.js
@@ -9,7 +9,38 @@ var friends = [
         name: "Jan",
         photo: "",
         scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "James",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "Jane",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "Jason",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "Jezebel",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "Jake",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
+    },
+    {
+        name: "Jamie",
+        photo: "",
+        scores: [5, 1, 4, 4, 3, 5, 2, 3, 4, 2]
     }
+
 ];
 
 //exporting the friends data
